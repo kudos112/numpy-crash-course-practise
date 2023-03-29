bfd27978e81bdc88e702c41b10c14ca61d3cc2b4
Abdul Quddous <quddoux112@gmail.com>
2023-03-30 00:57:57 +0500

    basics complete

---
+++ 1-basics.py
@@ -0,0 +1,37 @@
+# import numpy as np
+
+# return current version of numpy you're using
+# print(np.__version__)
+
+# # creates 1-d array
+# a = np.array([1,2,3])
+# print(a)
+# # return array shape in (columns, rows)
+# print(a.shape)
+# # return data type
+# print(a.dtype)
+# # return dimension of the array as it is 1 dimesional array
+# print(a.ndim)
+# # return array size
+# print(a.size)
+# # return size of each element as its data type is int64 mean 64 bits = 8 byte
+# print(a.itemsize)
+# return value at index a[0]
+# print(a[0])
+# # set the value at specific index
+# a[0] = 10
+
+# # Some Basic Mathematical operations
+# a = [1, 2, 3]
+# print(a + 2)
+# print(a - 2)
+# print(a * 2)
+# print(a / 2)
+
+# # Some Mathematical operations between Arrays
+# # a = [1, 2, 3]
+# b = [4, 5, 6]
+# print(b + a)
+# print(b - a)
+# print(b * a)
+# print(b / a)
