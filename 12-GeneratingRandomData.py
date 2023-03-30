1cbfb18ab983d14e85625f4e97c1f6c20fb32e2b
Abdul Quddous <quddoux112@gmail.com>
2023-03-31 01:47:33 +0500

    generating random data or random array

---
+++ 12-GeneratingRandomData.py
@@ -0,0 +1,29 @@
+# import numpy as np
+
+# # array = matrix
+
+# # generate array of random number between 0 and 1
+# a = np.random.random((2, 2))
+# print(a)
+# print(a.mean(), a.var())
+
+# # generate array of random number
+# a = np.random.randn(2, 2)
+# print(a)
+# print(a.mean(), a.var())
+
+# # # generate array of random integers number
+# a = np.random.randint(5, size=(3,)) # within 0-5
+# print(a) 
+
+# # # generate array of random integers number
+# a = np.random.randint(3, 5, size=(2, 2)) # within 3-5
+# print(a)
+
+# # generate array of random number of your choice
+# a = np.random.choice(3, size=(2, 2)) # within 0-2
+# print(a)
+
+# # generate array of random number of your choice
+# a = np.random.choice([-5, 10, 20, -50], size=(2, 2)) # within 0-2
+# print(a)
