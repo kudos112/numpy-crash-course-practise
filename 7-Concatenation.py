21c3f327e7e872adc774c8d584ba8597a36781e2
Abdul Quddous <quddoux112@gmail.com>
2023-03-30 14:44:46 +0500

    refactored the concatenation file

--- 7-Concatenation.py
+++ 7-Concatenation.py
@@ -1,4 +1,4 @@
-# import numpy as np
+import numpy as np
 
 # # Array Concatenation
 
@@ -7,20 +7,25 @@
 # print(a)  # => [1 2 3 4]
 # print(b)  # => [5 6 7 8]
 
-# by default np.concatenate has axis = 0
+# # by default np.concatenate has axis = 0
 # print(np.concatenate((a, b))) # added b as new row
-# [[ 1  2  3  4]
-#  [ 9 10 11 12]
-#  [ 4  5  7  8]]
+# # [[ 1  2  3  4]
+# #  [ 9 10 11 12]
+# #  [ 4  5  7  8]]
 # print(np.concatenate((a, b), axis=0))  # same to line 11
 
-# axis = 0 means add as row
-# axis = 1 means add as column
+# # Lets try with axis = None
+# # axis=None will transform all the rows into a 1-d Array
+# print(np.concatenate((a, b), axis=None))
+# # [ 1  2  3  4  9 10 11 12  4  5  6  7]
 
-# with axis = 1
-# returns error because we cannot add row of size 4 as column of size 2 in a
+# # axis = 0 means add as row
+# # axis = 1 means add as column
+
+# # with axis = 1
+# # returns error because we cannot add row of size 4 as column of size 2 in a
 # print(np.concatenate((a, b), axis=1))
-# using transpose of b also return error because column of elements 4 cannot be added to column of size 
+# # using transpose of b also return error because column of elements 4 cannot be added to column of size 
 # print(np.concatenate((a, b.T), axis=1))
 
 # # now try with matrix of having only column with size 2
@@ -28,9 +33,9 @@
 # print(np.concatenate((a, c), axis=1))
 # # output will be
 # # [[ 1  2  3  4 45]
-# #  [ 9 10 11 12 50]]
+# # [ 9 10 11 12 50]]
 
-# Transpose means turns rows into columns and columns into rows
+# # Transpose means turns rows into columns and columns into rows
 
 # # now try with matrix of having only column with size 2
 # c = np.array([[45, 50]])
