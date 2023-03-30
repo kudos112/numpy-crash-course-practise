6d338292dcde6e12020e0928a015d960f63aaf10
Abdul Quddous <quddoux112@gmail.com>
2023-03-30 14:37:37 +0500

    broadcasting

---
+++ 8-Broadcasting.py
@@ -0,0 +1,29 @@
+# import numpy as np
+
+# Broadcasting
+
+# a = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+# b = np.array([[1, 2, 3]])
+# c = a + b
+# print(c)
+# # output
+# # [[ 2  4  6]
+# #  [ 5  7  9]
+# #  [ 8 10 12]]
+
+# # same case as above
+# a = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+# b = np.array([[1, 2, 3], [1, 2, 3], [1, 2, 3]])
+# c = a + b
+# print(c)
+# # output
+# # [[ 2  4  6]
+# #  [ 5  7  9]
+# #  [ 8 10 12]]
+
+# # Try yourself and find out the behavior and reason 
+# a = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+# b = np.array([[1, 2, 3], [1, 2, 3]])
+# c = a + b
+# print(c)
+
